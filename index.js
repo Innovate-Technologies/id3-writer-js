2c9f9233db8b99b99383209cffe59f8954cdc214
Maarten Eyskens <meyskens@me.com>
2016-02-03 16:32:12 +0100

    Convert to ES5

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-require("babel-polyfill");
-require("babel-register");
-
-
-module.exports.write = (frames) => {
+module.exports.write = function (frames) {
 
     var id3Buffers = []
 
@@ -13,19 +9,22 @@ module.exports.write = (frames) => {
     // extended header not present (in this version)
 
     var id3FrameBuffers = []
-    for (var frame of frames) {
-        id3FrameBuffers.push(new Buffer(frame.name)) // header
-        var contentBuffer = new Buffer(frame.content, "utf8")
-        id3FrameBuffers.push(new Buffer("000000", "hex")) // set frame tags
-        var lengthString = (frame.content.length + 2).toString(16)
-        if (lengthString.length % 2 !== 0) {
-            lengthString = "0" + lengthString
+    for (var id in frames) {
+        if (frames.hasOwnProperty(id)) {
+            var frame = frames[id]
+            id3FrameBuffers.push(new Buffer(frame.name)) // header
+            var contentBuffer = new Buffer(frame.content, "utf8")
+            id3FrameBuffers.push(new Buffer("000000", "hex")) // set frame tags
+            var lengthString = (frame.content.length + 2).toString(16)
+            if (lengthString.length % 2 !== 0) {
+                lengthString = "0" + lengthString
+            }
+            id3FrameBuffers.push(new Buffer(lengthString, "hex")) // add length
+            id3FrameBuffers.push(new Buffer("0000", "hex")) // status flags
+            id3FrameBuffers.push(new Buffer("03", "hex")) // UTF-8 (what else?)
+            id3FrameBuffers.push(contentBuffer) // content
+            id3FrameBuffers.push(new Buffer("00", "hex")) // end UTF-8
         }
-        id3FrameBuffers.push(new Buffer(lengthString, "hex")) // add length
-        id3FrameBuffers.push(new Buffer("0000", "hex")) // status flags
-        id3FrameBuffers.push(new Buffer("03", "hex")) // UTF-8 (what else?)
-        id3FrameBuffers.push(contentBuffer) // content
-        id3FrameBuffers.push(new Buffer("00", "hex")) // end UTF-8
     }
 
     var framesBuffer = Buffer.concat(id3FrameBuffers)
